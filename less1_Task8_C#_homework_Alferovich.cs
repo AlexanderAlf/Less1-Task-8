d1c8c6c40db3d2ed05e293b21c6d02c15dd50d7a
AlexanderA <alexander.alferovich@gmail.com>
2023-01-11 13:19:22 +0400

    Final commit

--- less1_Task8_C#_homework_Alferovich.cs
+++ less1_Task8_C#_homework_Alferovich.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Пожалуйста, введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int size = 1;
+while (size <= num)
+{
+    if (size % 2 == 0)
+{
+    Console.WriteLine($"{size}");
+}
+size++;
+}
